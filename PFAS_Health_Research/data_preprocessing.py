d1990c84a19078680dfe19893beff8a37f0e7a0f
Idhant Jena <ijena@uci.edu>
2025-06-17 17:42:01 -0700

    Preprocessed CA PFAS data and aggregated it by county and well type

--- PFAS_Health_Research/data_preprocessing.py
+++ PFAS_Health_Research/data_preprocessing.py
@@ -15,3 +15,32 @@ low_birth_weight_california_data = california_data.iloc[:, [
 ]]
 low_birth_weight_california_data.columns = ["FIPS","County", "% Low Birth Weight", " Low Birth Weight National Z Score"]
 low_birth_weight_california_data.to_csv("low_birth_weight_california_data.csv", index=False)
+
+
+file_path= r"C:\Users\idhan\Downloads\pfas_health_issues\data\PFAS_CA_GW.csv"
+pfas_df = pd.read_csv(file_path)
+pfas_df = pfas_df[pfas_df["gm_gis_county"].notna()]
+
+desired_columns = [
+    'PFBTA', 'PFPA', 'PFHA', 'PFHPA', 'PFOA', 'PFNA', 'PFNDCA', 'PFUNDCA',
+    'PFDOA', 'PFTRIDA', 'PFTEDA', 'PFHXDA', 'PFODA', '3:3FTCA', '5:3FTCA', '7:3FTCA',
+    '4:2FTS', '6:2FTS', '8:2FTS', '10:2FTS', 'PFBSA', 'PFPES', 'PFHXSA', 'PFHPSA',
+    'PFOS', 'PFNS', 'PFDSA', 'PFOSA', 'ETFOSE', 'ETFOSA', 'NETFOSAA', 'MEFOSE',
+    'MEFOSA', 'NMEFOSAA', 'ADONA', 'HFPA-DA', '11ClPF3OUDS', '9ClPF3ONS',
+    'PFAS_total', 'gm_well_id', 'gm_well_category', 'gm_gis_county', 'gm_dataset_name'
+]
+
+desired_cols = [col for col in pfas_df.columns if col in desired_columns]
+filtered_pfas_df = pfas_df[desired_cols]
+
+chemical_columns = [
+    'PFBTA', 'PFPA', 'PFHA', 'PFHPA', 'PFOA', 'PFNA', 'PFNDCA', 'PFUNDCA',
+    'PFDOA', 'PFTRIDA', 'PFTEDA', 'PFHXDA', 'PFODA', '3:3FTCA', '5:3FTCA', '7:3FTCA',
+    '4:2FTS', '6:2FTS', '8:2FTS', '10:2FTS', 'PFBSA', 'PFPES', 'PFHXSA', 'PFHPSA',
+    'PFOS', 'PFNS', 'PFDSA', 'PFOSA', 'ETFOSE', 'ETFOSA', 'NETFOSAA', 'MEFOSE',
+    'MEFOSA', 'NMEFOSAA', 'ADONA', 'HFPA-DA', '11ClPF3OUDS', '9ClPF3ONS',
+    'PFAS_total'
+]
+
+aggregate_pfas_df = filtered_pfas_df.groupby(['gm_gis_county','gm_well_category'])[chemical_columns].mean().reset_index()
+aggregate_pfas_df.to_csv("california_aggregate_pfas_data.csv", index = False)
